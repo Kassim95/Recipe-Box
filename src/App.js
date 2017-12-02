c0812e89c198cae0a92f8223783a5e6a92b67b62
Kassim Ouattara <ouat.kassim@gmail.com>
2017-12-02 04:06:37 -0500

    Adding first react component with hard coded recipes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from 'react-bootstrap/lib/Accordion';
+import Panel from 'react-bootstrap/lib/Panel';
+import Button from 'react-bootstrap/lib/Button';
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
+import Modal from 'react-bootstrap/lib/Modal';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import FormControl from 'react-bootstrap/lib/FormControl';
+
 
 class App extends Component {
+  state = {
+    recipes: [
+      {recipeName: 'Kassim', ingredients: ['Sexy1', 'Champ', 'Smart']},
+      {recipeName: 'Kassim2', ingredients: ['Sexy2', 'Champ', 'Smart']},
+      {recipeName: 'Kassim3', ingredients: ['Sexy3', 'Champ', 'Smart']}
+    ]
+  }
+
   render() {
+    const {recipes} = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <Accordion>
+          {recipes.map((recipe, index) => (
+            <Panel header={recipe.recipeName} eventKey={index} key={index}>
+              <ol>
+                {recipe.ingredients.map((item) => 
+                <li key={item}>{item}</li>
+                )}
+              </ol>
+              <ButtonToolbar>
+                <Button bsStyle="danger">Delete Recipe</Button>
+                <Button bsStyle="default">Edit Recipe</Button>
+              </ButtonToolbar>
+            </Panel>
+          ))}
+        </Accordion>
       </div>
     );
   }
